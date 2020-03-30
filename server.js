91ffc956c370e653d5278ccb7b331bee7dcad6bb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-30 19:03:36 +0700

    Create a new server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    response.end('Hi, selamat datang di Node.js');
+});
+
+server.listen(3000);
+
+console.log('Server running on http://localhost:3000');
