aac63e1bba1cc6a6a0e09942065025caa1491683
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-30 19:16:07 +0700

    Getting url for routing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write('{"message" : "Hello, World!"}');
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('URL: ' + request.url);
     response.end();
 });
 
